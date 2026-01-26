91d67b9a1a72d658a5742f77e013eba7b9f7f44f
SilentIllusion <90772055+SilentIllusion@users.noreply.github.com>
2026-01-25 22:02:48 -0500

    Made some cosmetic changes

--- Module_1/personal_website/run.py
+++ Module_1/personal_website/run.py
@@ -4,15 +4,15 @@ app = Flask(__name__, template_folder='app/templates', static_folder='app/static
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', active_page='home')
 
 @app.route('/projects')
 def projects():
-    return render_template('projects.html')
+    return render_template('projects.html', active_page='projects')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', active_page='contact')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
